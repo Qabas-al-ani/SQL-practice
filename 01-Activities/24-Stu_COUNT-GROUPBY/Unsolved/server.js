14cfae30376179c5860ce0a3e7938fb016521353
Qabas al ani <doctorqa@yahoo.com>
2021-08-19 16:50:41 -0400

    added comment to each function of the equested queries

--- 01-Activities/24-Stu_COUNT-GROUPBY/Unsolved/server.js
+++ 01-Activities/24-Stu_COUNT-GROUPBY/Unsolved/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+// importing mysql module
 const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
@@ -6,7 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
+//  create database to start the function
 const db = mysql.createConnection(
   {
     host: 'localhost',
@@ -17,15 +18,15 @@ const db = mysql.createConnection(
   console.log(`Connected to the books_db database.`)
 );
 
-
+// ask query to get responses for the total count
 db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
   console.log(results);
 });
-
+// ask query to get responses for the total count for the sum of the results
 db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
   console.log(results);
 });
-
+// if the response is not found 404
 app.use((req, res) => {
   res.status(404).end();
 });
