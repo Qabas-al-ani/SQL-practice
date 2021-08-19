93669ecd6a5d827236e99f03ccec5b60c4db462a
Qabas al ani <doctorqa@yahoo.com>
2021-08-19 12:38:06 -0400

    cteated two seperate tables for one schema that contains product and catigories that each of them has more thsn one column

--- 01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
+++ 01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
@@ -1,1 +1,23 @@
 -- Write your schema here --
+DROP DATABASE IF EXISTS inventory_db;
+
+CREATE DATABASE inventory_db;
+
+use inventory_db;
+
+DROP TABLE IF EXISTS products;
+
+CREATE TABLE products (
+    id INT NOT NULL,
+    product_name VARCHAR(30) NOT NULL,
+    category_name VARCHAR(100) NOT NULL,
+    price INT NOT NULL,
+    in_stock BOOLEAN NOT NULL
+);
+
+DROP DATABASE IF EXISTS categories;
+
+CREATE TABLE categories (
+    id INT NOT NULL,
+    category_name VARCHAR(30) NOT NULL
+);
