8a600889ed07e3c5be92a0e621fd2e3603dbb6f2
Qabas al ani <doctorqa@yahoo.com>
2021-08-19 11:00:28 -0400

    added comment to the givin functions to discribe how they work

--- 01-Activities/08-Stu_CRUD-Insert/Unsolved/db/insert-select.sql
+++ 01-Activities/08-Stu_CRUD-Insert/Unsolved/db/insert-select.sql
@@ -1,7 +1,10 @@
+-- this function will affect the the id and the name of the biographies with the new VALUES givin
 INSERT INTO biographies (id, name)
 VALUES
     ( 001, "Diary of Anne Frank"),
     ( 002, "Frida: A Biography of Frida Kahlo"),
     ( 003, "Long Walk to Freedom");
 
+
+-- this will select all  from in biographies
 SELECT * FROM biographies;
