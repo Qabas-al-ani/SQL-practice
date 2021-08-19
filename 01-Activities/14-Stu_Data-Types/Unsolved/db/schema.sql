6a393c4a6010a9e0872ac2a12f1271c52dc0f69f
Qabas al ani <doctorqa@yahoo.com>
2021-08-19 12:14:19 -0400

    created table for customers and set it up not to equal NULL

--- 01-Activities/14-Stu_Data-Types/Unsolved/db/schema.sql
+++ 01-Activities/14-Stu_Data-Types/Unsolved/db/schema.sql
@@ -4,8 +4,8 @@ CREATE DATABASE customer_db;
 USE customer_db;
 
 CREATE TABLE customers (
-  id,
-  first_name,
-  last_name,
-  value_card_member
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  value_card_member BOOLEAN NOT NULL
 );
