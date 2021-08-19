92d5324b66904c111107789cb3e5ed678c4b3f82
Qabas al ani <doctorqa@yahoo.com>
2021-08-19 12:02:37 -0400

    added comment to the server.js function to discribe each function and what it does

--- 01-Activities/12-Stu_Connect-Node/Unsolved/server.js
+++ 01-Activities/12-Stu_Connect-Node/Unsolved/server.js
@@ -1,26 +1,32 @@
 const express = require('express');
+// import requiring mysql module
 const mysql = require('mysql2');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
+//  experss middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// connect to database
 const db = mysql.createConnection(
   {
     host: 'localhost',
+    // mysql username
     user: 'root',
+    //  Mysql password
     password: '',
     database: 'classlist_db'
   },
   console.log(`Connected to the classlist_db database.`)
 );
 
+// Query database
 db.query('SELECT * FROM students', function (err, results) {
   console.log(results);
 });
-
+// if the response is not found 404
 app.use((req, res) => {
   res.status(404).end();
 });
